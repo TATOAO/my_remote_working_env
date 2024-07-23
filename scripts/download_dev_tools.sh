aea452e868cc5c7f8e19fb30836582b99f5bcd89
TATOAO <liangwentaoss@126.com>
2024-07-23 15:07:04 +0800

    ready to install python

--- scripts/download_dev_tools.sh
+++ scripts/download_dev_tools.sh
@@ -7,7 +7,8 @@ mkdir -p $basic_devs
 
 absolute_path=$(realpath "$basic_devs")
 
-for pkg in autoconf automake bison flex gcc gcc-c++ gdb gettext libtool make patch pkgconfig redhat-rpm-config rpm-build rpm-sign byacc cscope ctags diffstat doxygen elfutils gcc-gfortran git indent intltool patchutils rcs subversion swig systemtap; do
-	yum install --installroot=$absolute_path --downloadonly --downloaddir $basic_devs $pkg || true
+for pkg in autoconf automake bison flex gcc gcc-c++ gdb gettext libtool make patch pkgconfig redhat-rpm-config rpm-build rpm-sign byacc cscope ctags diffstat doxygen elfutils gcc-gfortran git indent intltool patchutils rcs subversion swig systemtap zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel; 
+do
+	yum install -y --installroot=$absolute_path --downloadonly --downloaddir $basic_devs $pkg || true
 done
 
---
+++ scripts/download_python.sh
@@ -0,0 +1,8 @@
+
+# Load environment variables from .env file
+set -e
+# Navigate to the directory where the script is located
+source scripts/load_env.sh
+
+
+mkdir -p $python_path
