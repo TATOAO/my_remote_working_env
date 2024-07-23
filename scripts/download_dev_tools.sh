a45ffe76334cfb451f7892fae6c1ad455e0adb6a
TATOAO <liangwentaoss@126.com>
2024-07-23 14:10:13 +0800

    change to yum install downloadonly

--- scripts/download_dev_tools.sh
+++ scripts/download_dev_tools.sh
@@ -7,6 +7,7 @@ mkdir -p $basic_devs
 cd $basic_devs
 
 for pkg in autoconf automake bison flex gcc gcc-c++ gdb gettext libtool make patch pkgconfig redhat-rpm-config rpm-build rpm-sign byacc cscope ctags diffstat doxygen elfutils gcc-gfortran git indent intltool patchutils rcs subversion swig systemtap; do
-	yumdownloader --resolve $pkg
+	yum install --installroot=$basic_devs --downloadonly --downloaddir $basic_devs $pkg
 done
 
+# for pkg in autoconf automake bison flex gcc gcc-c++ gdb gettext libtool make patch pkgconfig redhat-rpm-config rpm-build rpm-sign byacc cscope ctags diffstat doxygen elfutils gcc-gfortran git indent intltool patchutils rcs subversion swig systemtap; do
