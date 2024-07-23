059e793a9fecfad661792c70dd33c17a19477c14
TATOAO <liangwentaoss@126.com>
2024-07-23 15:11:09 +0800

    python install

--- scripts/download_python.sh
+++ scripts/download_python.sh
@@ -4,5 +4,6 @@ set -e
 # Navigate to the directory where the script is located
 source scripts/load_env.sh
 
-
 mkdir -p $python_path
+
+wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz -P $python_path
