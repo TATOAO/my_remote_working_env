3cfb540145737ef8cf14500f9a670d9029e59818
TATOAO <liangwentaoss@126.com>
2024-07-23 14:47:58 +0800

    debug

--- scripts/download_dev_tools.sh
+++ scripts/download_dev_tools.sh
@@ -4,9 +4,8 @@ set -e
 source scripts/load_env.sh
 
 mkdir -p $basic_devs
-cd $basic_devs
 
-absolute_path=$(realpath {$basic_devs})
+absolute_path=$(realpath "$basic_devs")
 
 for pkg in autoconf automake bison flex gcc gcc-c++ gdb gettext libtool make patch pkgconfig redhat-rpm-config rpm-build rpm-sign byacc cscope ctags diffstat doxygen elfutils gcc-gfortran git indent intltool patchutils rcs subversion swig systemtap; do
 	yum install --installroot=$absolute_path --downloadonly --downloaddir $basic_devs $pkg || true
